8ff6a768c1fdb5ecbc12ae512c8f0604e4e26631
Nick <nmelms92@gmail.com>
2023-02-18 21:15:28 -0800

    switch to express

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  let path = req.url === "/" ? "/index.html" : req.url;
-  fs.readFile(__dirname + path, (err, content) => {
-    if (err) {
-      if (err.code === "ENOENT") {
-        console.log("we aint got it");
-        fs.readFile(__dirname + "/404.html", (err, content) => {
-          if (err) throw err;
-          res.end(content);
-        });
-      }
-    } else {
-      res.end(content);
-    }
-  });
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-server.listen(8080, () => {
-  console.log(`Server running at : 8080`);
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
 });
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.all("*", (req, res) => [
+  res.status(404).sendFile(path.join(__dirname, "/404.html")),
+]);
+
+app.listen(8080);
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/html");
+//   let path = req.url === "/" ? "/index.html" : req.url;
+//   fs.readFile(__dirname + path, (err, content) => {
+//     if (err) {
+//       if (err.code === "ENOENT") {
+//         console.log("we aint got it");
+//         fs.readFile(__dirname + "/404.html", (err, content) => {
+//           if (err) throw err;
+//           res.end(content);
+//         });
+//       }
+//     } else {
+//       res.end(content);
+//     }
+//   });
+// });
+
+// server.listen(8080, () => {
+//   console.log(`Server running at : 8080`);
+// });
